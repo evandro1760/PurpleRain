332ae7bc4924e515a7a8bee480034579ce916a41
evandro1760 <evandro.1760@gmail.com>
2018-05-31 08:44:02 -0300

    300 points

---
+++ rain.js
@@ -0,0 +1,35 @@
+
+function createPurple(purples, num){
+    
+    let purple = document.createElement('div');
+    
+    purple.classList.add('purple');
+    purple.classList.add('p' + num);
+    purples.appendChild(purple);
+
+    return purple;
+}
+
+function dropPurple(purple){
+    
+    let top = Math.random() * 60;
+    let left = Math.random() * 95;
+
+    let timer = setInterval(function(){
+        purple.style.top = top + '%';
+        purple.style.left = left + '%';
+        top += 0.5;
+        if(top >= 90){
+            top = Math.random() * 60;
+            left = Math.random() * 95;
+        }
+    }, 16);
+}
+
+
+let purples = document.querySelector('.purples');
+let n = 300;
+
+for(let i = 0; i < n; i++){
+    dropPurple(createPurple(purples, i));
+}
